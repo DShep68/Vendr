83448760b182dccb0271acf1e8f3061595ba6e5c
Deion G. Shepherd <dgs02997@lincoln.sjfc.edu>
2020-04-15 17:54:54 -0400

    dynamic adding and subtracting cart

--- Vendr/VendrJS/Cart.js
+++ Vendr/VendrJS/Cart.js
@@ -1,4 +1,41 @@
 
+/*
+var itemOne = document.getElementById('number').value;
+var itemTwo = document.getElementById('number1').value;
+var itemThree = document.getElementById('number2').value;
+var itemFour = document.getElementById('number3').value;
+
+
+
+if(itemOne == ""){
+    itemOne = "0";
+    
+    x[0].style.display = "none";
+
+}
+
+
+if(itemTwo == ""){
+    itemTwo = "0";
+
+    x[1].style.display = "none";
+}
+
+if(itemThree == ""){
+    itemThree = "0";
+
+    x[2].style.display = "none";
+}
+
+if(itemFour == ""){
+    itemFour = "0";
+
+    x[3].style.display = "none";
+}
+*/
+
+var x = document.getElementsByClassName("cart-item-row");
+
 function quantUpdaterItem1() {
     var x = document.getElementById("number").value;
     document.getElementById("item1Inventory").innerHTML = 'x' + x ;
@@ -26,17 +63,50 @@ function quantUpdaterItem1() {
   }
   
   function findTotal(){
-    var itemOne = document.getElementById('number').value;
-    var itemTwo = document.getElementById('number1').value;
-    var itemThree = document.getElementById('number2').value;
-    var itemFour = document.getElementById('number3').value;
-  
-    var itemOne = parseFloat(itemOne);
+
+    itemOne = document.getElementById('number').value;
+    itemTwo = document.getElementById('number1').value;
+    itemThree = document.getElementById('number2').value;
+    itemFour = document.getElementById('number3').value;
     
-    var itemTwo = parseFloat(itemTwo);
+    if(itemOne > 0){       
+        x[0].style.display = "flex"; 
+        itemOne = document.getElementById('number').value;
+    } else if(itemOne == "" || itemOne == 0){
+        itemOne = "0";
+        x[0].style.display = "none";
+    }
+    
+    if(itemTwo > 0){
+        x[1].style.display = "flex";
+        itemTwo = document.getElementById('number1').value;
+    } else if(itemTwo == "" || itemTwo == 0){
+        itemTwo = "0";
+        x[1].style.display = "none";
+    }
+    
+    if(itemThree > 0){
+        x[2].style.display = "flex";
+        itemThree = document.getElementById('number2').value;
+    } else if(itemThree == "" || itemThree == 0){
+        itemThree = "0";
+        x[2].style.display = "none";
+    }
+    
+    if(itemFour > 0){
+        x[3].style.display = "flex";
+        itemFour = document.getElementById('number3').value;
+    } else if(itemFour == "" || itemFour == 0){
+        itemFour = "0";
+        x[3].style.display = "none";
+    }
+  
+    itemOne = parseFloat(itemOne);
+    
+    itemTwo = parseFloat(itemTwo);
     console.log(itemOne+itemTwo);
-    var itemThree = parseFloat(itemThree);
-    var itemFour = parseFloat(itemFour);
+    itemThree = parseFloat(itemThree);
+    itemFour = parseFloat(itemFour);
   
     var total = (itemOne*itemPriceOne) + (itemTwo*itemPriceTwo) + (itemThree*itemPriceThree) + (itemFour*itemPriceFour);
     //console.log(total);
