47724db7aaef7b5ed185c09e3d456b8aceab82d2
Nathaniel C. Tunningley <nct07874@lincoln.sjfc.edu>
2020-04-23 20:42:40 -0400

    added py script

--- Vendr/VendrPHP/History.php
+++ Vendr/VendrPHP/History.php
@@ -39,9 +39,28 @@
     $ordersID[] = $row["OrderID"];
     $ordersItemID[] = $row["ItemID"];
   }
- 
+
+
+$numberOfOrders = sizeof($orderID);
+setcookie("numOrders",$numberOfOrders);
+
+//need: itemID(item name), item quant, item price, order total.
+
+//grabbing info from item:
+$in = '(' . implode(',', $ordersItemID) .')';
+
+$sql = "SELECT item.ItemPrice,item.ItemName
+FROM item WHERE item.itemID IN " . $in;
+$result = mysqli_query($conn,$sql);
+while($row = mysqli_fetch_assoc($result)) {
+  $itemPrice[] = $row["ItemPrice"];
+  $itemName[] = $row["ItemName"];
+}
+
+$one = 1;
 
 ?>
+
 <html id="fullHD">
   <head>
     <meta charset="UTF-8">
@@ -66,7 +85,67 @@
       </div>
     </div>
     <div>
-      <button type="button" class="collapsible">Order#11111</button>
+      <button id="order0" type="button" class="collapsible"> <?php echo "Order #: " , $orderID[$numberOfOrders-1]; 
+      $tempOrderID = $orderID[$numberOfOrders-1]; 
+      //grabs itemID and total price from orders table where the orderID is =
+      $findOrderTotal = "SELECT TotalPrice, OrderItemQuanity FROM orders WHERE OrderID = '$orderID[$tempOrderID]';";
+      $resultOrders = mysqli_query($conn,$findOrderTotal);
+      while($row1 = mysqli_fetch_assoc($resultOrders)) {
+        $TotalPrice[] = $row1["TotalPrice"];
+        $OrderItemQuant[] = $row1["OrderItemQuanity"];}
+        echo $TotalPrice[0];?> 
+      </button>
+      <div class="content">
+        <div class="cart-column">
+          <div class="cart-module-row">
+            <div class="column">
+              <div class="cart-item-row">
+                <p class="cart-item-name">
+                  Pepsi
+                </p>
+                <p class="cart-item-quantity">
+                  x4
+                </p>
+                <p class="cart-item-price">
+                  $4
+                </p>
+              </div>
+              <div class="cart-item-row">
+                <p class="cart-item-name">
+                  Pepsi
+                </p>
+                <p class="cart-item-quantity">
+                  x4
+                </p>
+                <p class="cart-item-price">
+                  $4
+                </p>
+              </div>
+              <div class="cart-item-row">
+                <p class="cart-item-name">
+                  Pepsi
+                </p>
+                <p class="cart-item-quantity">
+                  x4
+                </p>
+                <p class="cart-item-price">
+                  $4
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="total-row">
+            <p class="total">
+              Total
+            </p>
+            <p class="total-price">
+              <?php echo $TotalPrice[0]; ?>
+            </p>
+          </div>
+        </div>
+    </div>
+    <div>
+      <button id="order1" type="button" class="collapsible"><?php echo "Order #: " , $orderID[$numberOfOrders-2]; ?></button>
       <div class="content">
         <div class="cart-column">
           <div class="cart-module-row">
@@ -117,7 +196,7 @@
         </div>
     </div>
     <div>
-      <button type="button" class="collapsible">Order#11111</button>
+      <button id="order2" type="button" class="collapsible"><?php echo "Order #: " , $orderID[$numberOfOrders-3]; ?></button>
       <div class="content">
         <div class="cart-column">
           <div class="cart-module-row">
@@ -167,8 +246,8 @@
           </div>
         </div>
     </div>
-    <div>
-      <button type="button" class="collapsible">Order#11111</button>
+    <div >
+      <button id="order3" type="button" class="collapsible"><?php echo "Order #: " , $orderID[$numberOfOrders-4]; ?></button>
       <div class="content">
         <div class="cart-column">
           <div class="cart-module-row">
@@ -218,8 +297,8 @@
           </div>
         </div>
     </div>
-    <div>
-      <button type="button" class="collapsible">Order#11111</button>
+    <div >
+      <button id="order4" type="button" class="collapsible"><?php echo "Order #: " , $orderID[$numberOfOrders-5]; ?></button>
       <div class="content">
         <div class="cart-column">
           <div class="cart-module-row">
@@ -269,8 +348,8 @@
           </div>
         </div>
     </div>
-    <div>
-      <button type="button" class="collapsible">Order#11111</button>
+    <div >
+      <button id="order5" type="button" class="collapsible"><?php echo "Order #: " , $orderID[$numberOfOrders-6]; ?></button>
       <div class="content">
         <div class="cart-column">
           <div class="cart-module-row">
@@ -320,8 +399,8 @@
           </div>
         </div>
     </div>
-    <div>
-      <button type="button" class="collapsible">Order#11111</button>
+    <div >
+      <button id="order6" type="button" class="collapsible"><?php echo "Order #: " , $orderID[$numberOfOrders-7]; ?></button>
       <div class="content">
         <div class="cart-column">
           <div class="cart-module-row">
@@ -371,8 +450,8 @@
           </div>
         </div>
     </div>
-    <div>
-      <button type="button" class="collapsible">Order#11111</button>
+    <div >
+      <button id="order7" type="button" class="collapsible"><?php echo "Order #: " , $orderID[$numberOfOrders-8]; ?></button>
       <div class="content">
         <div class="cart-column">
           <div class="cart-module-row">
@@ -422,8 +501,8 @@
           </div>
         </div>
     </div>
-    <div>
-      <button type="button" class="collapsible">Order#11111</button>
+    <div >
+      <button id="order8" type="button" class="collapsible"><?php echo "Order #: " , $orderID[$numberOfOrders-9]; ?></button>
       <div class="content">
         <div class="cart-column">
           <div class="cart-module-row">
@@ -473,59 +552,8 @@
           </div>
         </div>
     </div>
-    <div>
-      <button type="button" class="collapsible">Order#11111</button>
-      <div class="content">
-        <div class="cart-column">
-          <div class="cart-module-row">
-            <div class="column">
-              <div class="cart-item-row">
-                <p class="cart-item-name">
-                  Pepsi
-                </p>
-                <p class="cart-item-quantity">
-                  x4
-                </p>
-                <p class="cart-item-price">
-                  $4
-                </p>
-              </div>
-              <div class="cart-item-row">
-                <p class="cart-item-name">
-                  Pepsi
-                </p>
-                <p class="cart-item-quantity">
-                  x4
-                </p>
-                <p class="cart-item-price">
-                  $4
-                </p>
-              </div>
-              <div class="cart-item-row">
-                <p class="cart-item-name">
-                  Pepsi
-                </p>
-                <p class="cart-item-quantity">
-                  x4
-                </p>
-                <p class="cart-item-price">
-                  $4
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="total-row">
-            <p class="total">
-              Total
-            </p>
-            <p class="total-price">
-              $12
-            </p>
-          </div>
-        </div>
-    </div>
-    <div>
-      <button type="button" class="collapsible">Order#11111</button>
+    <div >
+      <button id="order9" type="button" class="collapsible"><?php echo "Order #: " , $orderID[$numberOfOrders-10]; ?></button>
       <div class="content">
         <div class="cart-column">
           <div class="cart-module-row">
@@ -577,4 +605,96 @@
     </div>
   </body>
   <script src="../VendrJS/History.js"></script>
+  <script>
+    //document.getElementById("order0").style.display="none";
+    var numberOfOrders = getCookie("numOrders");
+    console.log(numberOfOrders);
+
+    //do some math in the if statments
+    if(numberOfOrders==0){
+      document.getElementById("order0").style.display = "none";
+      document.getElementById("order1").style.display = "none";
+      document.getElementById("order2").style.display = "none";
+      document.getElementById("order3").style.display = "none";
+      document.getElementById("order4").style.display = "none";
+      document.getElementById("order5").style.display = "none";
+      document.getElementById("order6").style.display = "none";
+      document.getElementById("order7").style.display = "none";
+      document.getElementById("order8").style.display = "none";
+      document.getElementById("order9").style.display = "none";
+    }else if(numberOfOrders==1){
+      document.getElementById("order1").style.display = "none";
+      document.getElementById("order2").style.display = "none";
+      document.getElementById("order3").style.display = "none";
+      document.getElementById("order4").style.display = "none";
+      document.getElementById("order5").style.display = "none";
+      document.getElementById("order6").style.display = "none";
+      document.getElementById("order7").style.display = "none";
+      document.getElementById("order8").style.display = "none";
+      document.getElementById("order9").style.display = "none";
+    }else if(numberOfOrders==2){
+      document.getElementById("order2").style.display = "none";
+      document.getElementById("order3").style.display = "none";
+      document.getElementById("order4").style.display = "none";
+      document.getElementById("order5").style.display = "none";
+      document.getElementById("order6").style.display = "none";
+      document.getElementById("order7").style.display = "none";
+      document.getElementById("order8").style.display = "none";
+      document.getElementById("order9").style.display = "none";
+    }else if(numberOfOrders==3){
+      document.getElementById("order3").style.display = "none";
+      document.getElementById("order4").style.display = "none";
+      document.getElementById("order5").style.display = "none";
+      document.getElementById("order6").style.display = "none";
+      document.getElementById("order7").style.display = "none";
+      document.getElementById("order8").style.display = "none";
+      document.getElementById("order9").style.display = "none";
+    }else if(numberOfOrders==4){
+      document.getElementById("order4").style.display = "none";
+      document.getElementById("order5").style.display = "none";
+      document.getElementById("order6").style.display = "none";
+      document.getElementById("order7").style.display = "none";
+      document.getElementById("order8").style.display = "none";
+      document.getElementById("order9").style.display = "none";
+    }else if(numberOfOrders==5){
+      document.getElementById("order5").style.display = "none";
+      document.getElementById("order6").style.display = "none";
+      document.getElementById("order7").style.display = "none";
+      document.getElementById("order8").style.display = "none";
+      document.getElementById("order9").style.display = "none";
+    }else if(numberOfOrders==6){
+      document.getElementById("order6").style.display = "none";
+      document.getElementById("order7").style.display = "none";
+      document.getElementById("order8").style.display = "none";
+      document.getElementById("order9").style.display = "none";
+    }else if(numberOfOrders==7){
+      document.getElementById("order7").style.display = "none";
+      document.getElementById("order8").style.display = "none";
+      document.getElementById("order9").style.display = "none";
+    }else if(numberOfOrders==8){
+      document.getElementById("order8").style.display = "none";
+      document.getElementById("order9").style.display = "none";
+    }else if(numberOfOrders==9){
+      document.getElementById("order9").style.display = "none";
+    }else{
+      console.log( "you fucked this up bro");
+      document.getElementById("order0").style.display = "none";
+    }
+    //find the cookie
+    function getCookie(cname) {
+      var name = cname + "=";
+      var decodedCookie = decodeURIComponent(document.cookie);
+      var ca = decodedCookie.split(';');
+      for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+        }
+      }
+      return "";
+    }
+</script>
 </html>
---
+++ Vendr/VendrPy/vendingMachine.py
@@ -0,0 +1,124 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="10.200.2.17",
+  database = "vendr",
+  user="vendr",
+  passwd="vendr"
+)
+
+mycursor = mydb.cursor()
+
+#2.7 version-- vendingCode = raw_input("Please enter validation key below:\n")
+vendingCode = input("Please enter validation key below:\n")
+mycursor.execute("SELECT OrderID FROM validation WHERE VendingCode = %s", (vendingCode,))
+
+orderIDArr = mycursor.fetchall()
+#print(type(orderIDArr))
+#print(orderIDArr[0])
+
+orderIDTemp = orderIDArr[0]
+orderID = orderIDTemp[0]
+#print(type(orderID))
+#int(orderID)
+
+#print (orderID)
+mycursor.execute("SELECT ItemID FROM orders WHERE OrderID = %s", (orderID,))
+
+itemID = mycursor.fetchall()
+#for x in itemID:
+ # print(x)
+
+numItems = len(itemID)
+
+if numItems == 0:
+    print("uh-oh!~ no items were found :(")
+    
+elif numItems == 1:
+    itemOneTemp = itemID[0]
+    itemOne = itemOneTemp[0]
+    #print(itemOne)
+
+    mycursor.execute("SELECT ItemName FROM item WHERE ItemID = %s", (itemOne,))
+    itemNameArray = mycursor.fetchall()
+
+    itemNameOne = itemNameArray[0]
+
+    print("\n Dispensing your order of:", itemNameOne)
+    print("\n Thank you for your purchase have a nice day!")
+
+
+elif numItems == 2:
+    itemOneTemp = itemID[0]
+    itemOne = itemOneTemp[0]
+    #print(itemOne)
+
+    itemTwoTemp = itemID[1]
+    itemTwo = itemID[0]
+    #print(itemTwo)
+
+    mycursor.execute("SELECT ItemName FROM item WHERE ItemID = %s OR ItemID = %s", (itemOne,itemTwo,))
+    itemNameArray = mycursor.fetchall()
+
+    itemNameOne = itemNameArray[0]
+    itemNameTwo = itemNameArray[1]
+
+    print("\n Dispensing your order of:", itemNameOne, itemNameTwo,)
+    print("\n Thank you for your purchase have a nice day!")
+
+
+elif numItems == 3:
+    itemOneTemp = itemID[0]
+    itemOne = itemOneTemp[0]
+    #print(itemOne)
+
+    itemTwoTemp = itemID[1]
+    itemTwo = itemTwoTemp[0]
+    #print(itemTwo)
+
+    itemThreeTemp = itemID[2]
+    itemThree = itemThreeTemp[0]
+    #print(itemThree)
+
+    mycursor.execute("SELECT ItemName FROM item WHERE ItemID = %s OR ItemID = %s OR ItemID = %s", (itemOne,itemTwo,itemThree,))
+    itemNameArray = mycursor.fetchall()
+
+    itemNameOne = itemNameArray[0]
+    itemNameTwo = itemNameArray[1]
+    itemNameThree = itemNameArray[2]
+
+    print("\n Dispensing your order of: ", itemNameOne, itemNameTwo, itemNameThree)
+    print("\n Thank you for your purchase have a nice day!")
+
+elif numItems == 4:
+    itemOneTemp = itemID[0]
+    itemOne = itemOneTemp[0]
+    #print(itemOne)
+
+    itemTwoTemp = itemID[1]
+    itemTwo = itemTwoTemp[0]
+    #print(itemTwo)
+
+    itemThreeTemp = itemID[2]
+    itemThree = itemThreeTemp[0]
+    #print(itemThree)
+
+    itemFourTemp = itemID[3]
+    itemFour = itemFourTemp[0]
+    #print(itemFour)
+
+    mycursor.execute("SELECT ItemName FROM item WHERE ItemID = %s OR ItemID = %s OR ItemID = %s OR ItemID = %s ", (itemOne,itemTwo,itemThree,itemFour,))
+    itemNameArray = mycursor.fetchall()
+
+    itemNameOne = itemNameArray[0]
+    itemNameTwo = itemNameArray[1]
+    itemNameThree = itemNameArray[2]
+    itemNameFour = itemNameArray[3]
+
+    print("\n Dispensing your order of: ", itemNameOne, itemNameTwo, itemNameThree, itemNameFour)
+    print("\n Thank you for your purchase have a nice day!")
+
+
+
+#for x in itemNameArray:
+ # print(x)
